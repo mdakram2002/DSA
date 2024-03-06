6673cb4cb0f835fc0753344942092f5ce82bf0ef
Md <mdakram12022002@gmail.com>
2024-03-06 12:45:22 +0530

    Add file of permutation

---
+++ Recursion-Part3/permutation.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void solve(vector<int> nums, vector<vector<int>>& ans, int index){
+    //base case 
+    if(index >= nums.size()){ //if nums is out of vector range/size then return ans through ans.push_back(nums)
+        ans.push_back(nums);
+        return;
+    }
+    for(int j=index; j<nums.size(); j++){
+        swap(nums[index], nums[j]);
+        solve(nums, ans, index+1);
+        //backtracking
+        swap(nums[index], nums[j]);
+    }
+
+}
+
+vector<vector<int>> permutation(vector<int>& nums){
+    vector<vector<int>> ans;
+    int index = 0;
+    solve(nums, ans, index);
+    return ans;
+}
