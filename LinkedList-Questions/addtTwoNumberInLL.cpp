3430f79b8a81e1e57a0f56420ec472e281a03230
Md <mdakram12022002@gmail.com>
2024-06-14 20:53:08 +0530

    some changes in binarySearch

---
+++ LinkedList-Questions/addtTwoNumberInLL.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+class Node
+{
+
+public:
+    int data;
+    Node *prev;
+    Node *next;
+
+    // constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void addTwoNumber(Node* head){
+
+}
+
