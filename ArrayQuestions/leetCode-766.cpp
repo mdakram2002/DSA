d783833fa488246ff35b0694b83eff8c340e0f29
Md <mdakram12022002@gmail.com>
2024-12-17 16:51:48 +0530

    Implimentation of LeetCode Problems 766 and 1706

---
+++ ArrayQuestions/leetCode-766.cpp
@@ -0,0 +1,22 @@
+// 766. Toeplitz Matrix
+/*
+Given an m x n matrix, return true if the matrix is Toeplitz. Otherwise, return false.
+
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.
+*/
+
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        for(int i =1; i<m; i++){
+            for(int j = 1; j<n; j++){
+                if(matrix[i][j] != matrix[i-1][j-1])
+                return false;
+            }
+        }
+        return true;
+    }
+};
